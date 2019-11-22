4bfe790d7c9acac1fe6426ed1a6e7d648b03ed30
Christopher Klapp <christopher@klapp.name>
2019-11-22 11:04:11 -0500

    build: handle json inline

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,12 @@ module.exports = function (api) {
 	api.cache(true);
 
 	const presets = [ "@babel/preset-env", "@babel/preset-react" ];
-	const plugins = [ ];
+	const plugins = [ "inline-json-import", {} ];
+	const ignore = [ "./src/components/mocks" ];
 
 	return {
 		presets,
-		plugins
+		plugins,
+		ignore
 	};
 };
