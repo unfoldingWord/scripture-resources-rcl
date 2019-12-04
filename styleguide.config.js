251e92a1f3a22206234b507cb6db9f108ef81cea
RoyalSix <jay.a.scottjr@biola.edu>
2019-12-04 04:29:26 -0800

    attempt babel fix

--- styleguide.config.js
+++ styleguide.config.js
@@ -158,7 +158,7 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /\.js?$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           loader: 'babel-loader',
         },
