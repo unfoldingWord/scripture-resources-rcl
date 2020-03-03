8f80b8066e46aef4a145d229b2315b8f5ae4c556
Christopher Klapp <christopher@klapp.name>
2020-03-03 08:14:14 -0500

    organized and described core selections tests

---
+++ src/core/selections/selections.spec.js
@@ -0,0 +1,121 @@
+import {
+  selectionsFromQuoteAndString,
+} from './selections';
+
+describe('selectionsFromQuoteAndString', () => {
+  it('all occurrences', () => {
+    const input = {
+      quote: "λόγος",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: -1,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "λόγος", occurrence: 1, occurrences: 3 },
+      { text: "λόγος", occurrence: 2, occurrences: 3 },
+      { text: "λόγος", occurrence: 3, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('phrase at beginning', () => {
+    const input = {
+      quote: "ἐν ἀρχῇ ἦν",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 1,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ἐν", occurrence: 1, occurrences: 1 },
+      { text: "ἀρχῇ", occurrence: 1, occurrences: 1 },
+      { text: "ἦν", occurrence: 1, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('elipsis: repeating ending word preceding first.', () => {
+    const input = {
+      quote: "Θεὸς…λόγος",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 1,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "Θεὸς", occurrence: 1, occurrences: 1 },
+      { text: "λόγος", occurrence: 3, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('elipsis: simple, short', () => {
+    const input = {
+      quote: "ὁ λόγος…πρὸς",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 1,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ὁ", occurrence: 1, occurrences: 3 },
+      { text: "λόγος", occurrence: 1, occurrences: 3 },
+      { text: "πρὸς", occurrence: 1, occurrences: 1 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('elipsis: end on something with punctuation', () => {
+    const input = {
+      quote: "ὁ λόγος…Θεόν",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 2,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ὁ", occurrence: 2, occurrences: 3 },
+      { text: "λόγος", occurrence: 2, occurrences: 3 },
+      { text: "Θεόν", occurrence: 1, occurrences: 1 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('repeated phrase: first occurrence', () => {
+    const input = {
+      quote: "ὁ λόγος",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 1,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ὁ", occurrence: 1, occurrences: 3 },
+      { text: "λόγος", occurrence: 1, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('repeated phrase: second occurrence', () => {
+    const input = {
+      quote: "ὁ λόγος",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 2,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ὁ", occurrence: 2, occurrences: 3 },
+      { text: "λόγος", occurrence: 2, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+
+  it('repeated phrase: last occurrence', () => {
+    const input = {
+      quote: "ὁ λόγος",
+      string: "ἐν ἀρχῇ ἦν ὁ λόγος, καὶ ὁ λόγος ἦν πρὸς τὸν Θεόν, καὶ Θεὸς ἦν ὁ λόγος.",
+      occurrence: 3,
+    };
+    const output = selectionsFromQuoteAndString(input);
+    const expected = [
+      { text: "ὁ", occurrence: 3, occurrences: 3 },
+      { text: "λόγος", occurrence: 3, occurrences: 3 },
+    ];
+    expect(output).toStrictEqual(expected);
+  });
+});
