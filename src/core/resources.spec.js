ed944905ff20cfe99ea022812d5daf62097fa9c6
ancientTexts-net <zach.pennington@unfoldingWord.org>
2020-11-09 10:41:42 -0600

    Fix crashing if verse (book) does not exist

--- src/core/resources.spec.js
+++ src/core/resources.spec.js
@@ -1,7 +1,10 @@
-import { parseResourceLink } from './resources'
+import { parseResourceLink, extendProject, resourceFromResourceLink, getResourceManifest } from './resources'
+import { versesFromReferenceIdAndBooks, referenceIdFromReference } from '../components/parallel-scripture/helpers'
+import usfmJS from 'usfm-js';
 
 const config = { server: "https://bg.door43.org" };
 const reference = { bookId: '3jn', chapter: 1, verse: 1 };
+const reference2jn = { bookId: '2jn', chapter: 1, verse: 1 };
 
 const resourceExpectedValue = {
     "config": {
@@ -15,6 +18,67 @@ const resourceExpectedValue = {
     "tag": "master",
     "username": "ru_gl",
 }
+
+describe('parse and download resource', () => {
+    it('should be RLOB 3jn', async () => {
+        const resourceLink = `https://git.door43.org/ru_gl/ru_rlob`;
+        const book = await resourceFromResourceLink({ resourceLink, reference, config });
+        // console.log(content);
+        expect(book.projects.length).toBeGreaterThan(0);
+    })
+
+    it('should be RLOB 3jn verses', async () => {
+        const resourceLink = `https://git.door43.org/ru_gl/ru_rlob`;
+
+        // const resource = await resourceFromResourceLink({ resourceLink, reference, config });
+        // const book = extendProject({});
+        // const books = [book];
+        // let referenceId = referenceIdsFromBooks({ books })[0];
+        // versesFromReferenceIdAndBooks({ referenceId, books });
+
+
+        // const resource = parseResourceLink({ resourceLink, config, reference });
+        // const manifest = await getResourceManifest(resource);
+        // const projects = manifest.projects.map((project) =>
+        //     extendProject({
+        //         project, resource, reference,
+        //     }),
+        // );
+        // const json = await projects[1].parseUsfm();
+        // //console.log(json);
+        // const referenceId = referenceIdFromReference(reference);
+        // const verses = versesFromReferenceIdAndBooks({ referenceId, books: projects.books });
+        // console.log(verses);
+
+
+        const resource = await resourceFromResourceLink({ resourceLink, reference, config });
+        const file = await resource.project.file();
+        const book = await usfmJS.toJSON(file);
+        //console.log(book.headers[0].content);
+        expect(book.headers[0].content).toContain("3JN RU_RLOB ru_Russian_ltr Unlocked Literal Bible");
+    })
+
+
+    it('should be RLOB 2jn verses (empty)', async () => {
+        const resourceLink = `https://git.door43.org/ru_gl/ru_rlob`;
+
+        const resource = await resourceFromResourceLink({ resourceLink, reference2jn, config });
+        const file = await resource.project.file();
+        const book = await usfmJS.toJSON(file);
+        //console.log(book.headers[0].content);
+        expect(book.headers[0].content).toContain("3JN RU_RLOB ru_Russian_ltr Unlocked Literal Bible");
+    })
+})
+
+describe('parse and download resource', () => {
+    it('should be RLOB 2jn', async () => {
+        const resourceLink = `https://git.door43.org/ru_gl/ru_rlob`;
+        let content = await resourceFromResourceLink({ resourceLink, reference2jn, config });
+        //console.log(content.projects[1]);
+        expect(content.projects.length).toBeGreaterThan(0);
+    })
+})
+
 
 describe('parseResourceLink without books', () => {
     it('should be ru_rlob', () => {
