45644a1929c3aadcf2e01248049e4d8a2d26e4b5
mannycolon <colonmanuel7@gmail.com>
2020-11-12 10:16:01 -0500

    Removed unused props

--- src/components/resources/useRsrc.js
+++ src/components/resources/useRsrc.js
@@ -5,70 +5,64 @@ import useEffect from 'use-deep-compare-effect';
 
 import { resourceFromResourceLink } from '../../core';
 
-function useRsrc({
-  config,
-  reference,
-  resourceLink,
-}) {
-  const [resource, setResource] = useState({});
-  const [usfmJson, setUsfmJson] = useState(null);
-  const [projectIdentifier, setProjectIdentifier] = useState('');
+function useRsrc({ config, reference, resourceLink }) {
+	const [resource, setResource] = useState({});
+	const [usfmJson, setUsfmJson] = useState(null);
+	const [projectIdentifier, setProjectIdentifier] = useState('');
 
-  useEffect(() => {
-    resourceFromResourceLink({
-      resourceLink, reference, config,
-    }).then(
-      (_resource) => {
-        const __resource = _resource && deepFreeze(_resource);
-        setResource(__resource);
-      },
-    );
-  }, [resourceLink, reference, config]);
+	useEffect(() => {
+		resourceFromResourceLink({
+			resourceLink,
+			reference,
+			config,
+		}).then((_resource) => {
+			const __resource = _resource && deepFreeze(_resource);
+			setResource(__resource);
+		});
+	}, [resourceLink, reference, config]);
 
-  const parseUsfm = useCallback(async () => {
-    if (resource && resource.project) {
-      const { project } = resource;
+	const parseUsfm = useCallback(async () => {
+		if (resource && resource.project) {
+			const { project } = resource;
 
-      if (project.identifier !== projectIdentifier || !usfmJson) {
-        const json = await project.parseUsfm();
-        setUsfmJson(json);
-        setProjectIdentifier(project.identifier);
-      }
-    }
-  }, [projectIdentifier, resource, usfmJson]);
+			if (project.identifier !== projectIdentifier || !usfmJson) {
+				const json = await project.parseUsfm();
+				setUsfmJson(json);
+				setProjectIdentifier(project.identifier);
+			}
+		}
+	}, [projectIdentifier, resource, usfmJson]);
 
-  const getFile = useCallback(async () => {
-    const file = await resource.project?.file();
-    return file;
-  }, [resource]);
+	const getFile = useCallback(async () => {
+		const file = await resource.project?.file();
+		return file;
+	}, [resource]);
 
-  return {
-    state: resource,
-    actions: {
-      getFile,
-      parseUsfm,
-    },
-  };
+	return {
+		state: resource,
+		actions: {
+			getFile,
+			parseUsfm,
+		},
+	};
 }
 
 useRsrc.propTypes = {
-  /** the resource content */
-  resource: PropTypes.object,
-  /** reference: chapter and verse */
-  reference: PropTypes.object,
-  /** The link to parse and fetch the resource manifest */
-  resourceLink: PropTypes.string.isRequired,
-  /** The configuration of the server, and fetching */
-  config: PropTypes.shape({
-    server: PropTypes.string.isRequired,
-    /** the overriding cache settings */
-    cache: PropTypes.shape({
-      /** cache age in ms */
-      maxAge: PropTypes.number,
-    }),
-  }),
-  /** action taken after a resource is acquired */
-  onResource: PropTypes.func.isRequired,
+	props: PropTypes.shape({
+		/** The configuration of the server, and fetching */
+		config: PropTypes.shape({
+			server: PropTypes.string.isRequired,
+			/** the overriding cache settings */
+			cache: PropTypes.shape({
+				/** cache age in ms */
+				maxAge: PropTypes.number,
+			}),
+		}),
+		/** reference: chapter and verse */
+		reference: PropTypes.object,
+		/** The link to parse and fetch the resource manifest */
+		resourceLink: PropTypes.string.isRequired,
+	}),
 };
 
 export default useRsrc;
