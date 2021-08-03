f0d6d9d3820f32a67926940159efd1a7fc9eb0b8
Bruce McLean <mcleanb@photo-nomad.net>
2021-08-03 08:33:55 -0400

    add code coverage only for tests. disable testing in dist folder.

--- babel.prod.config.js
+++ babel.prod.config.js
@@ -1,16 +1,10 @@
+// build config for production (on publish)
 module.exports = {
   'presets': ['@babel/preset-env', '@babel/preset-react'],
   'plugins': [
     '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
     'inline-json-import',
-    [
-      'istanbul',
-      {
-        'include': ['src/**/**.js'],
-        'exclude': ['**/mocks/**'],
-      },
-    ],
   ],
   'ignore': ['**/*.usfm.js'],
 };
