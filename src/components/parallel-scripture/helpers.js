3fec3e57890c66835cf930e6914c6ff3cc656dfa
larsgson <4819617+larsgson@users.noreply.github.com>
2022-08-31 08:20:30 +0200

    parallel-scripture helpers conflict resolved

--- src/components/parallel-scripture/helpers.js
+++ src/components/parallel-scripture/helpers.js
@@ -1,15 +1,23 @@
-import { occurrenceInjectVerseObjects } from "../../core/selections/verseObjects";
+import { occurrenceInjectVerseObjects } from '../../core/selections/verseObjects';
 
 export const referenceIdsFromBooks = ({ books }) => {
   const referenceIds = new Set([]);
 
   books.forEach((book) => {
-    if (book && book.chapters) {
-      Object.keys(book.chapters).forEach((chapterKey) => {
-        const chapter = book.chapters[chapterKey];
+    if (book) {
+      let _chapters;
+      if ( book.chapters ) {
+        _chapters = book.chapters;
+      } else if ( book.json.chapters ) {
+        _chapters = book.json.chapters;
+      } else {
+        return;
+      }
+      Object.keys(_chapters).forEach((chapterKey) => {
+        const chapter = _chapters[chapterKey];
 
         Object.keys(chapter).forEach((verseKey) => {
-          const referenceId = chapterKey + ":" + verseKey;
+          const referenceId = chapterKey + ':' + verseKey;
           referenceIds.add(referenceId);
         });
       });
@@ -18,64 +26,79 @@ export const referenceIdsFromBooks = ({ books }) => {
   return [...referenceIds];
 };
 
-export const isVerseKeyInRange = ({ range, verseKey }) => {
-  verseKey = typeof verseKey === "string" ? parseInt(verseKey) : verseKey;
-  let [first, last] = range.split("-");
+export const isVerseKeyInRange = (({ range, verseKey }) => {
+  verseKey = (typeof verseKey === 'string') ? parseInt(verseKey) : verseKey;
+  let [first, last] = range.split('-');
   first = parseInt(first);
   last = parseInt(last);
   const inRange = first <= verseKey && verseKey <= last;
   return inRange;
-};
+});
 
-export const rangeFromVerseAndVerseKeys = ({ verseKeys, verseKey }) => {
-  const range = verseKeys.find((_verseKey) => {
-    if (_verseKey.includes("-")) {
-      // if the verseKey includes - it is a range
+export const rangeFromVerseAndVerseKeys = (({ verseKeys, verseKey }) => {
+  const range = verseKeys.find(_verseKey => {
+    if (_verseKey.includes('-')) { // if the verseKey includes - it is a range
       return isVerseKeyInRange({ range: _verseKey, verseKey });
     }
     return false;
   });
   return range;
-};
+});
 
 export const versesFromReferenceIdAndBooks = ({ referenceId, books }) => {
+  // console.log("versesFromReferenceIdAndBooks() referenceId,books=", referenceId,books)
   const versesData = books.map((book, index) => {
     const reference = referenceFromReferenceId(referenceId);
-    if (book && book.chapters && book.chapters.length > reference.chapter) {
-      //if (book) {
-
-      const chapterData = book.chapters[reference.chapter];
+    //if (book && book.chapters && book.chapters.length > reference.chapter) {
+    if (book) {
+      // this is odd... book is coming in with two different formats.
+      // One kind looks like this:
+      // {headers: Array(2), chapters: {…}}
+      // chapters: {1: {…}, 2: {…}, 3: {…}}
+      // headers: (2) [{…}, {…}]
+      //
+      // the other, which does not match the expected shape:
+      // {json: {…}, response: {…}}
+      // json:
+      // chapters:
+      // 1: {1: {…}, 2: {…}, 3: {…}, 4: {…}, 5: {…}, 6: {…}, 7: {…}, 8: {…}, 9: {…}, 10: {…}, 11: {…}, 12: {…}, 13: {…}, 14: {…}, 15: {…}, front: {…}}
+      // [[Prototype]]: Object
+      // headers: []
+      // [[Prototype]]: Object
+      // response: {data: {…},
+      // 
+      // So doing a bit of manipulation to provide the expected shape
+      // that does not have the "json" and "response" intermediate attributes
+      let _chapters;
+      if ( book.chapters ) {
+        _chapters = book.chapters;
+      } else if ( book.json.chapters ) {
+        _chapters = book.json.chapters;
+      } else {
+        return;
+      }
+      // console.log("index, book, and reference=", index, book, reference);
+      // console.log("chapterData=", _chapters[reference.chapter]);
+      const chapterData = _chapters[reference.chapter];
       let verseData = chapterData && chapterData[reference.verse];
       let range;
 
       if (!verseData && chapterData) {
         const verseKeys = Object.keys(chapterData);
-        range = rangeFromVerseAndVerseKeys({
-          verseKeys,
-          verseKey: reference.verse,
-        });
+        range = rangeFromVerseAndVerseKeys({ verseKeys, verseKey: reference.verse });
         verseData = chapterData[range];
       }
 
-      if (
-        index === 0 &&
-        verseData &&
-        verseData.verseObjects &&
-        verseData.verseObjects.length
-      ) {
+      if (index === 0 && verseData && verseData.verseObjects && verseData.verseObjects.length) {
         const _verseData = { ...verseData };
-        _verseData.verseObjects = occurrenceInjectVerseObjects(
-          _verseData.verseObjects
-        );
+        _verseData.verseObjects = occurrenceInjectVerseObjects(_verseData.verseObjects);
         verseData = _verseData;
       }
 
       let verseTitle = reference.verse;
 
-      if (!(verseTitle === "front" || verseTitle === "back")) {
-        verseTitle = range
-          ? reference.chapter + ":" + range
-          : reference.chapter + ":" + reference.verse;
+      if (!(verseTitle === 'front' || verseTitle === 'back')) {
+        verseTitle = range ? reference.chapter + ':' + range : reference.chapter + ':' + reference.verse;
       }
       return { verseData, verseTitle };
     }
@@ -85,11 +108,11 @@ export const versesFromReferenceIdAndBooks = ({ referenceId, books }) => {
 
 export const dataFromBooks = ({ books }) => {
   const referenceIds = referenceIdsFromBooks({ books });
-  const data = referenceIds.map((referenceId) => {
+  const data = referenceIds.map(referenceId => {
     let row = { referenceId };
 
     books.forEach((_, index) => {
-      const [chapterKey, verseKey] = referenceId.split(":");
+      const [chapterKey, verseKey] = referenceId.split(':');
       const chapterData = books[index].chapters[chapterKey];
       let verseData = chapterData[verseKey];
 
@@ -118,10 +141,7 @@ export const dataFromReference = ({ books, reference }) => {
 
     if (!verseData) {
       const verseKeys = Object.keys(chapterData);
-      const range = rangeFromVerseAndVerseKeys({
-        verseKeys,
-        verseKey: reference.verse,
-      });
+      const range = rangeFromVerseAndVerseKeys({ verseKeys, verseKey: reference.verse });
       verse = chapterData[range];
     }
 
@@ -134,10 +154,9 @@ export const dataFromReference = ({ books, reference }) => {
   return data;
 };
 
-export const referenceIdFromReference = (reference) =>
-  reference.chapter + ":" + reference.verse;
+export const referenceIdFromReference = (reference) => reference.chapter + ':' + reference.verse;
 
 export const referenceFromReferenceId = (referenceId) => {
-  const [chapter, verse] = referenceId.split(":");
+  const [chapter, verse] = referenceId.split(':');
   return { chapter, verse };
 };
